dd07013d61a37bde20051adc90834e7cd744b260
Nikita <semikin91@gmail.com>
2021-09-29 12:48:15 +0300

    changed paths

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,6 @@
 // Import my plugins
-import ieCheck from './plugins/_iecheck.js';
-import hoHashLinks from './plugins/_hohashlinks.js';
+import ieCheck from './components/_iecheck.js';
+import hoHashLinks from './components/_hohashlinks.js';
 
 // Import external plugins
 
